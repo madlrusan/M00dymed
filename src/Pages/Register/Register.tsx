4f0440a79b8c7521b1587692f412d6bd9a8cf7bc
Madalina Rusan <65464380+madlrusan@users.noreply.github.com>
2022-11-25 21:17:19 +0200

    register started

--- src/Pages/Register/Register.tsx
+++ src/Pages/Register/Register.tsx
@@ -1,9 +1,18 @@
-import { LogoContainer, StyledForm } from '../../components/common/CredentialsForm.components';
+import {
+    LogoContainer,
+    NameContainer,
+    NameInput,
+    StyledForm,
+} from '../../components/common/CredentialsForm.components';
 
 export const Register = () => {
     return (
         <StyledForm>
             <LogoContainer>LOGO</LogoContainer>
+            <NameContainer>
+                <NameInput id="filled-required" label="First Name" variant="filled" />
+                <NameInput id="filled-required" label="Last Name" variant="filled" />
+            </NameContainer>
         </StyledForm>
     );
 };
