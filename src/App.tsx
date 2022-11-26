d3b2c67d025391d9960228d7a41d1af8cbbbb4a4
Bianca Belea <bianca.belea@student.upt.ro>
2022-11-27 01:00:06 +0200

    Commit app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Layout from './components/containers/Layout';
-import {
-    SeparateViewWithDoctorMenu,
-    SeparateViewWithForm,
-    SeparateViewWithMedicationTable,
-} from './Pages/SeparateView/SeparateView';
+import { SeparateViewWithDoctorMenu, SeparateViewWithForm } from './Pages/SeparateView/SeparateView';
 
 const App = () => {
     return (
@@ -16,7 +12,6 @@ const App = () => {
                         <Route path="login" element={<SeparateViewWithForm form="Login" />} />
                         <Route path="register" element={<SeparateViewWithForm form="Register" />} />
                         <Route path="patients" element={<SeparateViewWithDoctorMenu />} />
-                        <Route path="medication" element={<SeparateViewWithMedicationTable />} />
                     </Route>
                 </Routes>
             </BrowserRouter>
