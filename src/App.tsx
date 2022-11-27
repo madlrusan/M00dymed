672f987b13301c267a08b14bbee8c72d56a63bbf
Madalina Rusan <65464380+madlrusan@users.noreply.github.com>
2022-11-27 05:13:36 +0200

    logic menu and path hopefully last one

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Layout from './components/containers/Layout';
@@ -6,17 +7,18 @@ import {
     SeparateViewWithForm,
     SeparateViewWithPatientMenu,
 } from './Pages/SeparateView/SeparateView';
+import { Appwrite } from './services/Appwrite';
 
 const App = () => {
-    // const { getRole } = Appwrite();
-    // const [role, setRole] = useState();
-    // useEffect(() => {
-    //     const funct = async () => {
-    //         const role = await getRole();
-    //         return role;
-    //     };
-    //     funct().then((r) => setRole(r));
-    // }, []);
+    const { getRole } = Appwrite();
+    const [role, setRole] = useState();
+    useEffect(() => {
+        const funct = async () => {
+            const role = await getRole();
+            return role;
+        };
+        funct().then((r) => setRole(r));
+    }, []);
     // const role = 1;
     return (
         <>
@@ -24,24 +26,27 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Layout />}>
                         <Route path="/" element={<SeparateViewWithForm form="Login" />} />
-                        {/* {role == 2 ? ( */}
-                        <>
-                            <Route path="register" element={<SeparateViewWithForm form="Register" />} />
-                            <Route path="patients" element={<SeparateViewWithDoctorMenu view="patients" />} />
-                            <Route path="addExercises" element={<SeparateViewWithDoctorMenu view="addExercises" />} />
+
+                        <Route path="register" element={<SeparateViewWithForm form="Register" />} />
+                        <Route path="patients" element={<SeparateViewWithDoctorMenu role={1} view="patients" />} />
+                        <Route
+                            path="addExercises"
+                            element={<SeparateViewWithDoctorMenu role={1} view="addExercises" />}
+                        />
+
+                        {role == 1 ? (
                             <Route
                                 path="seePatient/:email/:role"
-                                element={<SeparateViewWithDoctorMenu view="seePatient" id={'email'} />}
+                                element={<SeparateViewWithDoctorMenu view="seePatient" role={1} />}
                             />
-                        </>
-                        {/* ) : ( */}
-                        <>
-                            <Route path="exercises" element={<SeparateViewWithPatientMenu view="exercises" />} />
-                            <Route path="medication" element={<SeparateViewWithPatientMenu view="medication" />} />
-                        </>
-                        {/* )} */}
-
-                        {/* <Route path="editPatient/:id" element={<EditPatient />} /> */}
+                        ) : (
+                            <Route
+                                path="seePatient/:email/:role"
+                                element={<SeparateViewWithPatientMenu view="seePatient" role={0} />}
+                            />
+                        )}
+                        <Route path="exercises" element={<SeparateViewWithPatientMenu view="exercises" role={0} />} />
+                        <Route path="medication" element={<SeparateViewWithPatientMenu view="medication" role={0} />} />
                     </Route>
                 </Routes>
             </BrowserRouter>
--- src/Components/PatientMenu/PatientMenu.tsx
+++ src/Components/PatientMenu/PatientMenu.tsx
@@ -1,9 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { mediumPurple } from '../../modules/theme';
+import { Appwrite } from '../../services/Appwrite';
 import { Menu, SideMenu, RouteMenuItem } from '../DoctorMenu/DoctorMenu.components';
 
 export const PatientMenu = () => {
+    const [meUrl, setMeUrl] = useState('');
+    const { getUser } = Appwrite();
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        const funct = async () => {
+            const user = await getUser();
+            return user?.email;
+        };
+        funct().then((r) => setUser(r));
+        setMeUrl('/' + user + '/0');
+    }, []);
+    // setMeUrl('/' + user?.email + '/0');
     return (
         <SideMenu>
             <div style={{ marginTop: '-20vh', marginBottom: '20vh', display: 'block' }}>
@@ -14,7 +27,7 @@ export const PatientMenu = () => {
                 ></img>
             </div>
             <Menu>
-                <RouteMenuItem display="Me" path="#" />
+                <RouteMenuItem display="Me" path={meUrl} />
                 <RouteMenuItem display="Medication" path="/medication" />
                 <RouteMenuItem display="Exercises" path="/exercises" />
                 <RouteMenuItem display="Good Reading" path="#" />
